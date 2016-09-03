911bc1a6c44fd26eab9f7e6d5212c8f76a5a4181
Nico Hämäläinen <nico@sizeof.io>
2016-09-04 01:24:54 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    // Movement speed for player
+    public float movementSpeed;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+    // Update is called once per frame
+    void Update ()
+    {
+		// Get input axes
+        float horizontalAxis = Input.GetAxisRaw("Horizontal");
+        float verticalAxis = Input.GetAxisRaw("Vertical");
+
+        // Horizontal movement
+        if (horizontalAxis > 0.5f || horizontalAxis < -0.5f)
+        {
+			transform.Translate(new Vector3(
+				horizontalAxis * movementSpeed * Time.deltaTime,
+				0f,
+				0f
+			));
+        }
+
+        // Vertical movement
+        if (verticalAxis > 0.5f || verticalAxis < -0.5f)
+        {
+			transform.Translate(new Vector3(
+                0f,
+				verticalAxis * movementSpeed * Time.deltaTime,
+                0f
+			));
+        }
+    }
+}
