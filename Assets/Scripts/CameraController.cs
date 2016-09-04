63085e46409c8dc0f3f3f860c5c28da60add3cc9
Nico Hämäläinen <nico@sizeof.io>
2016-09-04 14:58:58 +0300

    Fixed pixel snapping for ground, added camera tracking

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject trackedObject;
+    private Vector3 trackedObjectPosition;
+
+    public float trackingSpeed;
+
+    // Use this for initialization
+    void Start () {
+            
+    }
+
+    // Update is called once per frame
+    void Update () {
+        trackedObjectPosition = new Vector3(
+            trackedObject.transform.position.x,
+            trackedObject.transform.position.y,
+            transform.position.z
+        );
+
+        transform.position = Vector3.Lerp(transform.position, trackedObjectPosition, trackingSpeed * Time.deltaTime);
+    }
+}
